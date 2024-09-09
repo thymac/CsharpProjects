9557a7a7021317d9d3a2c8851462b0c0ea7be07a
thymac <thywillberdone@gmail.com>
2024-09-09 15:07:36 +0300

    Perform operations on arrays using helper methods in C#

--- HelperMethods/Program.cs
+++ HelperMethods/Program.cs
@@ -19,10 +19,31 @@
 // Exercise - Explore Clear() and Resize()
 string[] pallets = { "B14", "A11", "B12", "A13" };
 Console.WriteLine("");
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 
+Console.WriteLine($"Before: {pallets[0].ToLower()}");
 Array.Clear(pallets, 0, 2);
+if (pallets[0] != null)
+  Console.WriteLine($"After: {pallets[0].ToLower()}");
+
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
   Console.WriteLine($"-- {pallet}");
 }
+Console.WriteLine("");
+
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+  Console.WriteLine(pallet ?? "null");
+}
+
+Console.WriteLine();
+string sentense = "The quick brown fox jumps over the lazy fox.";
+var arraySentense = sentense.Split(" ").ToArray();
+Console.WriteLine(arraySentense);
