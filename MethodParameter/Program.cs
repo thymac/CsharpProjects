c0b4df771c7bc0422200a7a85823424ffecf81f9
thymac <thywillberdone@gmail.com>
2024-09-29 22:44:32 +0300

    Create C# methods with parameters

--- MethodParameter/Program.cs
+++ MethodParameter/Program.cs
@@ -1,9 +1,33 @@
-﻿// Exercise - Use parameters in methods
+﻿// Add parameters to methods
+
+CountTo(5);
+
+void CountTo(int max)
+{
+  for (int i = 0; i < max; i++)
+  {
+    if (i == (max - 1))
+    {
+      Console.WriteLine($"{i}");
+    }
+    else
+    {
+      Console.Write($"{i}, ");
+    }
+  }
+}
+
+// Exercise - Use parameters in methods
+Console.Write(new string(' ', Console.BufferWidth));
 
 int[] schedule = { 800, 1200, 1600, 2000 };
 
+DisplayAdjustedTimes(schedule, 3, -12);
+
 void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
 {
+  int diff = 0;
+
   if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
   {
     Console.WriteLine("Invalid GMT");
@@ -19,7 +43,7 @@ void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
 
   for (int i = 0; i < times.Length; i++)
   {
-    int newTime = ((times[i] + diff)) % 2400;
+    int newTime = (times[i] + diff) % 2400;
     Console.WriteLine($"{times[i]} -> {newTime}");
   }
 }
