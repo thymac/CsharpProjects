03cc98d3f4580233cd3848f057571711ef528222
thymac <thywillberdone@gmail.com>
2024-10-01 16:40:43 +0300

    Create C# methods with parameters

--- MethodParameter/Program.cs
+++ MethodParameter/Program.cs
@@ -73,7 +73,7 @@ void PrintCircleInfo(int radius)
 {
   Console.WriteLine($"Circle with radius {radius}");
   PrintCircleArea(radius);
-  PrintCircleCircumfrence(radi us);
+  PrintCircleCircumfrence(radius);
 }
 
 void PrintCircleArea(int radius)
@@ -183,3 +183,37 @@ RSVP("Jonte", 2, "Stone fruit", false);
 ShowRSVPs();
 
 // Exercise - Complete the challenge to display email addresses
+
+string[,] corporate =
+{
+  { "Robert", "Bavin" }, { "Simon", "Bright" },
+  { "Kim", "Sinclair" }, { "Aashrita", "Kamath" },
+  { "Sarah", "Delucchi" }, { "Sinan", "Ali" },
+};
+
+string[,] external =
+{
+  { "Vinnie", "Ashton" }, { "Cody", "Dysart" },
+  { "Shay", "Lawrence" }, { "Daren", "Valdes" },
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+  // Display internal email address
+  DisplayEmail(first: corporate[i, 0], last: corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+  // Display external email address
+  DisplayEmail(first: external[i,0], last: external[i, 1], domain: externalDomain);
+}
+
+void DisplayEmail(string first, string last, string domain = "contoso.com")
+{
+  string email = first.Substring(0, 2) + last;
+  email = email.ToLower();
+  Console.WriteLine($"{email}@{domain}");
+}
