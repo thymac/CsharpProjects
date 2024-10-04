fc8eb5aa1323216313878512c3fbb658ba3da0c7
thymac <thywillberdone@gmail.com>
2024-10-04 12:25:20 +0300

    Create C# methods with parameters

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -94,83 +94,108 @@
 
 // Exercise - Build code with methods
 
-/*
-If ipAddress consists of 4 numbers
-and
-if each ipAddress number has no leading zeroes
-and
-if each ipAddress number is in range 0 - 255
+// /*
+// If ipAddress consists of 4 numbers
+// and
+// if each ipAddress number has no leading zeroes
+// and
+// if each ipAddress number is in range 0 - 255
 
-then ipAddress is valid
+// then ipAddress is valid
 
-else ipAddress is is invalid
-*/
+// else ipAddress is is invalid
+// */
 
-// string ipv4Input = "241.31.100.5";
-string[] ipv4Input = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...255" };
+// // string ipv4Input = "241.31.100.5";
+// string[] ipv4Input = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...255" };
 
-bool validLength = false;
-bool validZeroes = false;
-bool validRange = false;
+// bool validLength = false;
+// bool validZeroes = false;
+// bool validRange = false;
 
-foreach (string ip in ipv4Input)
+// foreach (string ip in ipv4Input)
+// {
+//   ValidateLength(ip);
+//   ValidateZeroes(ip);
+//   ValidateRange(ip);
+
+//   if (validLength && validZeroes && validRange)
+//   {
+//     Console.WriteLine($"{ip} is a valid IPV4 address\n");
+//   }
+//   else
+//   {
+//     Console.WriteLine($"{ip} is an invalid IPV4 address\n");
+//   }
+// }
+
+// void ValidateLength(string ip)
+// {
+//   string[] address = ip.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+//   validLength = address.Length == 4;
+
+//   if (!validLength)
+//     Console.WriteLine($"{ip} is of an invalid length.");
+// }
+
+// void ValidateZeroes(string ip)
+// {
+//   string[] address = ip.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+//   foreach (string number in address)
+//   {
+//     if (number.Length > 1 && number.StartsWith('0'))
+//     {
+//       Console.WriteLine($"{ip} has a number with an invalid leading '0'.");
+//       validZeroes = false;
+//       return;
+//     }
+//   }
+
+//   validZeroes = true;
+// }
+
+// void ValidateRange(string ip)
+// {
+//   string[] address = ip.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+//   int maxValue = 255;
+//   int value = 0;
+
+//   foreach (string number in address)
+//   {
+//     value = Convert.ToInt32(number);
+//     if (value > maxValue)
+//     {
+//       Console.WriteLine($"{ip} has a number that's in an invalid range.");
+//       validRange = false;
+//       return;
+//     }
+//   }
+
+//   validRange = true;
+// }
+
+
+// Exercise - Complete the challenge to create a reusable method
+
+Random random = new Random();
+int luck = random.Next(100);
+
+string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
+string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
+string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
+string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+
+TellFortune();
+
+void TellFortune()
 {
-  ValidateLength(ip);
-  ValidateZeroes(ip);
-  ValidateRange(ip);
-
-  if (validLength && validZeroes && validRange)
+  Console.WriteLine("A fortune teller whispers the following words:");
+  string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+  for (int i = 0; i < 4; i++)
   {
-    Console.WriteLine($"{ip} is a valid IPV4 address\n");
-  }
-  else
-  {
-    Console.WriteLine($"{ip} is an invalid IPV4 address\n");
-  }
-}
-
-void ValidateLength(string ip)
-{
-  string[] address = ip.Split('.', StringSplitOptions.RemoveEmptyEntries);
-
-  validLength = address.Length == 4;
-
-  if (!validLength)
-    Console.WriteLine($"{ip} is of an invalid length.");
-}
-
-void ValidateZeroes(string ip)
-{
-  string[] address = ip.Split('.', StringSplitOptions.RemoveEmptyEntries);
-  validZeroes = true;
-
-  foreach (string number in address)
-  {
-    if (number.Length > 1 && number.StartsWith('0'))
-    {
-      Console.WriteLine($"{ip} has a number with an invalid leading '0'.");
-      validZeroes = false;
-      return;
-    }
-  }
-}
-
-void ValidateRange(string ip)
-{
-  string[] address = ip.Split('.', StringSplitOptions.RemoveEmptyEntries);
-  validRange = true;
-
-  int maxValue = 255;
-  int value = 0;
-
-  foreach (string number in address)
-  {
-    value = Convert.ToInt32(number);
-    if (value > maxValue)
-    {
-      Console.WriteLine($"{ip} has a number that's in an invalid range.");
-      validRange = false;
-      return;
-    }
+    Console.Write($"{text[i]} {fortune[i]} ");
   }
 }
